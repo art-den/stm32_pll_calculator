bf13c771451c78c66217bc96eec02b787ef4e2d9
Denis <denis.artyomov@gmail.com>
2020-12-06 01:34:39 +0500

    Less `constexpr` in sources. Thanks Reflector from http://forum.easyelectronics.ru ! Minor improvements

--- stm32f4_pll_calculator.hpp
+++ stm32f4_pll_calculator.hpp
@@ -42,109 +42,96 @@ template <
 class stm32f4_pll_calculator
 {
 private:
-	enum class Type { N, M, P, Q };
+	enum class Factor { N, M, P, Q };
 
-	template <Type type, uint16_t q, uint16_t m, uint16_t p>
-	static constexpr uint16_t calc_by_q_m_p()
+	static constexpr uint16_t calc_by_q_m_p(Factor type, uint16_t q, uint16_t m, uint16_t p)
 	{
-		constexpr unsigned n = (uint64_t)SysclockFreq * (uint64_t)m * (uint64_t)p / (uint64_t)HseFreq;
-		if constexpr (n < MinN) return 0;
-		if constexpr (n > MaxN) return 0;
+		unsigned n = (uint64_t)SysclockFreq * (uint64_t)m * (uint64_t)p / (uint64_t)HseFreq;
+		if (n < MinN) return 0;
+		if (n > MaxN) return 0;
 
-		constexpr unsigned test_sysfreq = (uint64_t)HseFreq * (uint64_t)n / (m * p);
-		if constexpr (test_sysfreq != SysclockFreq)
+		unsigned test_sysfreq = (uint64_t)HseFreq * (uint64_t)n / (m * p);
+		if (test_sysfreq != SysclockFreq)
 			return 0;
 
-		constexpr unsigned test_usbfreq = (uint64_t)HseFreq * (uint64_t)n / (m * q);
-		if constexpr (test_usbfreq != UsbFreq)
+		unsigned test_usbfreq = (uint64_t)HseFreq * (uint64_t)n / (m * q);
+		if (test_usbfreq != UsbFreq)
 			return 0;
 
-		if constexpr (type == Type::N)
-			return n;
-
-		else if constexpr (type == Type::M)
-			return m;
-
-		else if constexpr (type == Type::P)
-			return p;
-
-		else if constexpr (type == Type::Q)
-			return q;
-
-		return 0;
+		return
+			(type == Factor::N) ? n :
+			(type == Factor::M) ? m :
+			(type == Factor::P) ? p :
+			(type == Factor::Q) ? q :
+			0;
 	}
 
-	template <Type type, uint16_t q, uint16_t m>
-	static constexpr uint16_t calc_by_q_m()
+	static constexpr uint16_t calc_by_q_m(Factor type, uint16_t q, uint16_t m)
 	{
-		constexpr unsigned vco_in_freq = HseFreq / m;
-		if constexpr (vco_in_freq < MinInVcoFreq) return 0;
-		if constexpr (vco_in_freq > MaxInVcoFreq) return 0;
+		unsigned vco_in_freq = HseFreq / m;
+		if (vco_in_freq < MinInVcoFreq) return 0;
+		if (vco_in_freq > MaxInVcoFreq) return 0;
 
-		constexpr auto res2 = calc_by_q_m_p<type, q, m, 2>();
-		if constexpr (res2 != 0) return res2;
+		auto res2 = calc_by_q_m_p(type, q, m, 2);
+		if (res2 != 0) return res2;
 
-		constexpr auto res4 = calc_by_q_m_p<type, q, m, 4>();
-		if constexpr (res4 != 0) return res4;
+		auto res4 = calc_by_q_m_p(type, q, m, 4);
+		if (res4 != 0) return res4;
 
-		constexpr auto res6 = calc_by_q_m_p<type, q, m, 6>();
-		if constexpr (res6 != 0) return res6;
+		auto res6 = calc_by_q_m_p(type, q, m, 6);
+		if (res6 != 0) return res6;
 
-		return calc_by_q_m_p<type, q, m, 8>();
+		return calc_by_q_m_p(type, q, m, 8);
 	}
 
-	template <Type type, uint16_t q, uint16_t m, uint16_t m_max>
-	static constexpr uint16_t calc_by_q_m_loop()
+	static constexpr uint16_t calc_by_q_m_loop(Factor type, uint16_t q, uint16_t m, uint16_t m_max)
 	{
-		constexpr auto res = calc_by_q_m<type, q, m>();
-		if constexpr (res != 0)
+		auto res = calc_by_q_m(type, q, m);
+		if (res != 0)
 			return res;
-		else if constexpr (m < m_max)
-			return calc_by_q_m_loop<type, q, m + 1, m_max>();
+		else if (m < m_max)
+			return calc_by_q_m_loop(type, q, m + 1, m_max);
 		return 0;
 	}
 
-	template <Type type, uint16_t q>
-	static constexpr uint16_t calc_by_q()
+	static constexpr uint16_t calc_by_q(Factor type, uint16_t q)
 	{
-		constexpr unsigned VcoFreq = UsbFreq * q;
-		if constexpr (VcoFreq < MinVcoFreq) return 0;
-		if constexpr (VcoFreq > MaxVcoFreq) return 0;
+		unsigned VcoFreq = UsbFreq * q;
+		if (VcoFreq < MinVcoFreq) return 0;
+		if (VcoFreq > MaxVcoFreq) return 0;
 
-		return calc_by_q_m_loop<type, q, 2, 63>();
+		return calc_by_q_m_loop(type, q, 2, 63);
 	}
 
-	template <Type type, uint16_t q, uint16_t q_max>
-	static constexpr uint16_t calc_by_q_loop()
+	static constexpr uint16_t calc_by_q_loop(Factor type, uint16_t q, uint16_t q_max)
 	{
-		constexpr auto res = calc_by_q<type, q>();
-		if constexpr (res != 0)
+		auto res = calc_by_q(type, q);
+		if (res != 0)
 			return res;
-		else if constexpr (q < q_max)
-			return calc_by_q_loop<type, q + 1, q_max>();
+		else if (q < q_max)
+			return calc_by_q_loop(type, q + 1, q_max);
 
 		return 0;
 	}
 
-	template <Type type>
+	template <Factor type>
 	static constexpr uint16_t calc()
 	{
-		constexpr uint16_t result = calc_by_q_loop<type, 2, 15>();
+		constexpr uint16_t result = calc_by_q_loop(type, 2, 15);
 		static_assert(result, "Combination of SysclockFreq, HseFreq and UsbFreq is incorrect");
 		return result;
 	}
-
 public:
 
 	// Division factor for the main PLL (PLL) input clock
-	static constexpr uint16_t pll_m = calc<Type::M>();
+	static constexpr uint16_t pll_m = calc<Factor::M>();
 
 	// Main PLL (PLL) multiplication factor for VCO
-	static constexpr uint16_t pll_n = calc<Type::N>();
+	static constexpr uint16_t pll_n = calc<Factor::N>();
 
 	// Main PLL (PLL) division factor for main system clock
-	static constexpr uint16_t pll_p = calc<Type::P>();
+	static constexpr uint16_t pll_p = calc<Factor::P>();
 
 	// Main PLL (PLL) division factor for USB OTG FS, and SDIO clocks
-	static constexpr uint16_t pll_q = calc<Type::Q>();
+	static constexpr uint16_t pll_q = calc<Factor::Q>();
 };
